2f90c1d164be1c4b085b7bf50aadfbe984767c05
nyyanja <nyanja.ramahavatra@gmail>
2025-09-03 12:09:57 +1100

    Feat(api): add exo 3,e(Bonus)

--- main.py
+++ main.py
@@ -34,5 +34,15 @@ def car_detail(id: int):
     if id  not in car_list():
         return {"error": "Car not found"},404
 
+@app.put("/cars/{id}/characteristics")
+def update_car_characteristics(id: int, max_speed: int, max_fuel_capacity: int):
+    if id  not in car_list():
+        return {"error": "Car not found"},404
+    if max_speed <= 0 or max_fuel_capacity <= 0:
+        return {"error": "Invalid characteristics"},400
+    return {"id": id,
+            "max_speed": max_speed,
+            "max_fuel_capacity": max_fuel_capacity},200
+
 
 
